dd708e5fd3abd1440d96637ebf91bdee3a6a2c6c
hman <hman@student.codam.nl>
2023-02-22 15:05:00 +0100

    Updated autoindex page build function - added right align for the size - added the '/' on directories - clean up the code a little and added comments

--- src/html.cpp
+++ src/html.cpp
@@ -9,7 +9,6 @@ namespace webserv {
 		std::string page_buffer;
 
 		page_buffer += "<html>\n<head><title>Index of " + dir_name + "</title></head>\n";
-		//page_buffer += "<h1>Index of " + dir_name + "</h1><hr><pre><a href=\"../\">../</a>\n";
 		page_buffer += "<h1>Index of " + dir_name + "</h1><hr>\n";
 
 		DIR *dir;
@@ -17,36 +16,43 @@ namespace webserv {
 		if ((dir = opendir (dir_path.c_str())) == NULL)
 		{
 			std::cerr << "Can't open directory: " << dir_path << std::endl;
+			//maybe sent a 500 Internal Server Error?
 		}
 		else
 		{
-			page_buffer += "<table><tr><th>Name</th><th>Last Modified</th><th>Size</th></tr>";
+			page_buffer += "<table><tr><th>Name</th><th>Last Modified</th><th>Size (bytes)</th></tr>";
 			while ((ent = readdir(dir)) != NULL)
 			{
 				if (std::string(ent->d_name) == ".")
 					continue ;
 
-				std::string filepath = dir_path + "/";
-				if(ent->d_type != DT_DIR){
-					filepath += ent->d_name;
+				std::string filepath = dir_path;
+				if(dir_path.back()!= '/'){
+					filepath += '/';
 				}
-				struct stat buf;
+				filepath += ent->d_name;
+
+				//get directory/file information
+				struct stat buf; //struct to store file/directory data
 				stat(filepath.c_str(), &buf);
-				char timeline[80];
+				char timeline[80]; //string to store c-style string for the time of last modified
 				strftime(timeline,80,"%D %r", localtime(&buf.st_mtimespec.tv_sec));
+
 				page_buffer += "<tr><td>";
 				page_buffer += "<a href=\"";
 				page_buffer += ent->d_name;
 				if (ent->d_type == DT_DIR)
-					page_buffer += '/';
+					page_buffer += "/";
 				page_buffer += "\">";
 				page_buffer += ent->d_name;
+				if (ent->d_type == DT_DIR)
+					page_buffer += "/";
 				page_buffer += "</a></td><td>";
-				page_buffer += timeline; // TODO: file size & date
+				page_buffer += timeline;
 				if (ent->d_type != DT_DIR)
-					page_buffer += "</td><td>" + std::to_string(buf.st_size) + "</td>";
+					page_buffer += "</td><td align=\"right\">" + std::to_string(buf.st_size) + "</td>";
 				else
-					page_buffer += "</td><td> - </td>";
+					page_buffer += "</td><td align=\"right\"> - </td>";
 				page_buffer += '\n';
 			}
 			closedir(dir);
