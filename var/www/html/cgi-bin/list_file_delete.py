22f1e08b89a4f1f3be7409267e03d925d6b78003
hman <hman@student.codam.nl>
2023-03-27 18:16:17 +0200

    working version of the delete file cgi and adapted the connection.cpp to return a html page succesfull delete

--- var/www/html/cgi-bin/list_file_delete.py
+++ var/www/html/cgi-bin/list_file_delete.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/local/bin/python3
 import cgi, sys, os
 import cgitb; cgitb.enable()
 
@@ -6,27 +6,32 @@ print("content-type: text/html\n\n")
 print("<!doctype html>\n")
 print("<html>")
 print("<body>")
-print("<p align=\"center\">Welcome ! </p>")
+print("<h1>Directory listing of /upload/ </h1>")
 print("<table>")
 
 upload_dir = os.environ['UPLOAD_DIRECTORY'];
 files = os.listdir(upload_dir)
-
+if len(files) == 0:
+	print("<tr><td> Directory is empty</td></tr>")
 for file in files:
 	print("<tr>")
 	print("<td>" + file + "</td>")
-	print("<td> <div id=\"del_button\"><button type=\"button\" onclick=\"loadXMLDoc()\">Delete file</button></div></td>")
+	print("<td> <div id=\"del_button\"><button type=\"button\" onclick=\"loadXMLDoc(\'" + file + "\')\">Delete file</button></div></td>")
 	print("</tr>")
 
 print("</table>")
 print("<script>")
-print("function loadXMLDoc(){")
+print("function loadXMLDoc(filename){")
 print("var xhttp = new XMLHttpRequest();")
 print("xhttp.onreadystatechange = function(){")
-print("if (this.readyState == 4 && this.status == 200){")
-print("document.getElementById(\"del_button\").innerHTML = this.responseText;")
-print("}};")
-print("xhttp.open(\"GET\", \"/\", true)")
+print("console.log(\"the staus is:\" + this.status + \" \" + this.readyState)")
+print("		if (this.readyState == 4){")
+print("				document.open();")
+print("				document.write(this.responseText);")
+print("				document.close();")
+print("		}")
+print("};")
+print("xhttp.open(\"DELETE\", \"/files/\" + filename, true)")
 print("xhttp.send()}")
 print("</script>")
 print("</body>")
